c79790f2dac80192d04bad3a0f5d1bd569ebf7dd
YAMADA Kazuaki <iterable.company@gmail.com>
2023-02-12 16:54:11 +0900

    itertools diff_with

---
+++ itertools/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    diff_with_();
+}
+
+use itertools::{diff_with, Diff};
+fn diff_with_() {
+    match diff_with(vec![1, 2, 3], vec![2, 4, 6], |i, j| 2 * i == *j) {
+        None => (),
+        _ => panic!("!!!"),
+    }
+
+    match diff_with(vec![1, 2, 3], vec![1, 3, 2], |i, j| i == j) {
+        Some(Diff::FirstMismatch(idx, i, j)) => {
+            println!("i: {:?}, j: {:?}", i, j);
+            assert_eq!(idx, 1)
+        }
+        _ => panic!("!!!"),
+    }
+
+    match diff_with(vec![1, 2, 3], vec![2, 4], |i, j| 2 * i == *j) {
+        Some(Diff::Shorter(idx, i)) => {
+            println!("i: {:?}", i);
+            assert_eq!(idx, 2)
+        }
+        _ => panic!("!!!"),
+    }
+
+    match diff_with(vec![1, 2], vec![2, 4, 6], |i, j| 2 * i == *j) {
+        Some(Diff::Longer(idx, i)) => {
+            println!("i: {:?}", i);
+            assert_eq!(idx, 2)
+        }
+        _ => panic!("!!!"),
+    }
+}
